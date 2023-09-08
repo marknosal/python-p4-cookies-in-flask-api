ab0ece5783322871b4d056c88ff73bbd92fa6732
Mark Nosal <mark.c.nosal@gmail.com>
2023-09-08 12:14:03 -0500

    finish code along

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, session, jsonify, make_response
 app = Flask(__name__)
 app.json.compact = False
 
+
 app.secret_key = b'?w\x85Z\x08Q\xbdO\xb8\xa9\xb65Kj\xa9_'
 
 @app.route('/sessions/<string:key>', methods=['GET'])
